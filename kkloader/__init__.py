e2a0a0bbb33a9b5d6c33abd6fab99c5965445d9b
多数 <40860726+great-majority@users.noreply.github.com>
2023-08-13 19:59:45 +0000

    support Honeycome data

--- kkloader/__init__.py
+++ kkloader/__init__.py
@@ -1,5 +1,14 @@
-from .KoikatuCharaData import *  # noqa isort: skip
+from .KoikatuCharaData import (  # noqa isort: skip
+    Custom as kk_Custom,
+    Coordinate as kk_Coordinate,
+    Parameter as kk_Parameter,
+    About as kk_About,
+    Status as kk_Status,
+    KKEx as kk_KKEx,
+    KoikatuCharaData,
+)
 from .KoikatuSaveData import KoikatuSaveData  # noqa isort: skip
 from .EmocreCharaData import EmocreCharaData  # noqa
 from .EmocreMapData import EmocreMapData  # noqa
 from .EmocreSceneData import EmocreSceneData  # noqa
+from .HoneycomeCharaData import HoneycomeCharaData  # noqa
